9afe4a2f742a56a00a8e225776a5b2791b61f68f
Volkov85 <xplz2007@gmail.com>
2020-07-08 18:28:05 +0600

    Загрузка данных через AJAX

--- js/map.js
+++ js/map.js
@@ -1,12 +1,28 @@
 'use strict';
 
 window.map = (function () {
+  /**
+   * Возвращает массив с добавлением индексного объекта и подготавливает к выводу фотографий
+   * @param {Array} array - исходный массив
+   * @return {Array} array - дополненный массив
+   */
+  var extendArray = function (array) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i].offer.photos.length === 0) {
+        array[i].offer.photos = false;
+      }
+      array[i].index = i;
+    }
+    return array;
+  };
+
   return {
     /**
      * Заполнение блока card DOM-элементами на основе массива JS-объектов
      * @param  {Array} advert - элемент массива объектов, содержащий сгенерированные данные для карточки
      */
     pushCard: function (advert) {
+      extendArray(advert);
       var fragment = document.createDocumentFragment();
       var listElement = document.querySelector('.map');
       var filterElement = document.querySelector('.map__filters-container');
@@ -21,6 +37,7 @@ window.map = (function () {
      * @param  {Array} ads - массив объектов, содержащий сгенерированные данные для пина
      */
     pushPins: function (ads) {
+      extendArray(ads);
       var fragment = document.createDocumentFragment();
       var listElement = document.querySelector('.map__pins');
       ads.forEach(function (card) {
