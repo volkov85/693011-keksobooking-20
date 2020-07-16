7070bcc1179bb6a7ae13a223526e767a0b005c4d
Volkov85 <xplz2007@gmail.com>
2020-07-17 00:27:27 +0600

    Модуль загрузки аватарки и фото жилья

---
+++ js/avatar.js
@@ -0,0 +1,53 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = [
+    'gif',
+    'jpg',
+    'jpeg',
+    'png'
+  ];
+
+  var fileChooserAvatar = document.querySelector('.ad-form__field input[type=file]');
+  var fileChooserPhoto = document.querySelector('.ad-form__upload input[type=file]');
+  var previewAvatar = document.querySelector('.ad-form-header__preview img');
+  var previewPhoto = document.querySelector('.ad-form__photo');
+
+  fileChooserAvatar.addEventListener('change', function () {
+    var file = fileChooserAvatar.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        previewAvatar.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+  fileChooserPhoto.addEventListener('change', function () {
+    var file = fileChooserAvatar.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        previewPhoto.style = 'background: center / cover no-repeat url("' + reader.result + '");';
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
